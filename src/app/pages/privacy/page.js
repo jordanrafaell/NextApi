0872c088173756e39d2d587e2877706f0a5e06a6
Jordan Rafael <61181764+jordanrafaell@users.noreply.github.com>
2024-09-14 16:28:33 -0300

    criação das paginas de Política de Privacidade e Termos de Serviço

--- src/app/pages/privacy/page.js
+++ src/app/pages/privacy/page.js
@@ -1,12 +1,13 @@
+'use client';
 import libs from '../../utils/libs/libs.js';
 import '../../utils/import-css/style.js';
 
-const { Script, React} = libs;
+const { Script, React } = libs;
 
-export default function Privacy(){
+export default function Privacy() {
   return (
     <>
-    <div>Hello world</div>
+      <div>Hello world</div>
     </>
-  )
+  );
 }
--- src/app/pages/services/page.js
+++ src/app/pages/services/page.js
@@ -1,9 +1,10 @@
+'use client'
 import libs from '../../utils/libs/libs.js';
 import '../../utils/import-css/style.js';
 
 const { Script, React } = libs;
 
-export default function Privacy() {
+export default function Services() {
   return (
     <>
       <div>Hello world</div>
